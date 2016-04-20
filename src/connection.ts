640327656d01369fd73a194812fa9d29a2c59a59
Sn0wFox <ruben.pericas@laposte.net>
2016-04-20 19:33:57 +0200

    Deleted connection.ts : this class is specific

--- src/connection.ts
+++
@@ -1,6 +0,0 @@
-import {Connection} from "palantiri-interfaces";
-import {EventEmitter} from "events";
-
-export class OChatConnection extends EventEmitter implements Connection {
-  connected: boolean;
-}
