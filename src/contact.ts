575600a65c9bf8fe77cbe1a6ee5ee28df1b39498
Sn0wFox <ruben.pericas@laposte.net>
2016-04-21 19:59:11 +0200

    Updated PalantiriContact

--- src/contact.ts
+++ src/contact.ts
@@ -1,102 +1,24 @@
 import * as Bluebird from "bluebird";
 
 import {Contact} from "palantiri-interfaces";
-import {ContactAccount} from "palantiri-interfaces";
 
-export class OChatContact implements Contact {
+export class PalantiriContact implements Contact {
   fullname: string;
 
-  nicknames: string[];
+  protocol: string;
 
-  accounts: ContactAccount[];
+  localID: number;
 
-  getAccounts(): Bluebird<ContactAccount[]> {
-    return Bluebird.resolve(this.accounts);
-  }
+	isCompatibleWith(protocol: string): boolean {
+		return this.protocol.toLowerCase() === protocol.toLowerCase();
+	}
 
-  getNicknames(): string[] {
-    return this.nicknames;
-  }
-
-  getPrincipalName(): string {
+  getFullname(): string {
     return this.fullname;
   }
 
-  setPrincipalName(newPrincipalName: string): Bluebird.Thenable<Contact> {
+  setFullname(newPrincipalName: string): Bluebird.Thenable<Contact> {
     this.fullname = newPrincipalName;
-	  return Bluebird.resolve(this);
-  }
-
-  mergeContacts(contact: Contact, callback?: (err: Error, succes: Contact) => any): Bluebird.Thenable<Contact> {
-    let error: Error = null;
-    let numberOfErrors: number = 0;
-    for(let contactAccount of contact.accounts) {
-      this.addAccount(contactAccount, (err, acc) => {
-        if(err) {
-          numberOfErrors++;
-        }
-      });
-    }
-    if(numberOfErrors === contact.accounts.length) {
-      error = new Error("Unable to merge contact. Maybe the second was part of the current.");
-    } else if(numberOfErrors !== 0) {
-      error = new Error(numberOfErrors + " account of the contact in parameters could not be added to the current contact.");
-    }
-    if(callback) {
-      callback(error, this);
-    }
-    return Bluebird.resolve(this);
-  }
-
-  unmergeContacts(contact: Contact, callback?: (err: Error, succes: Contact) => any): Bluebird.Thenable<Contact> {
-    let error: Error = null;
-    for(let contactAccount of contact.accounts) {
-      this.removeAccount(contactAccount, (err, acc) => {
-        if(err) {
-          error = new Error("Unable to unmerge contact. One account in the parameters is not part of the current Contact.");
-        }
-      });
-      if(error)
-      {
-        break;
-      }
-    }
-    if(callback) {
-      callback(error, this);
-    }
-    return Bluebird.resolve(this);
-  }
-
-  addAccount(account: ContactAccount, callback? : (err: Error, succes: ContactAccount[]) => any): Bluebird.Thenable<Contact> {
-    let index: number = this.accounts.indexOf(account);
-    let err: Error = null;
-    if(index === -1) {
-      this.nicknames.push(account.contactName);
-      if(!this.fullname) {
-        this.fullname = account.contactName;
-      }
-      this.accounts.push(account);
-    } else {
-      err = new Error("This account already exists for this contact.");
-    }
-    if(callback) {
-      callback(err, this.accounts);
-    }
-	  return Bluebird.resolve(this);
-  }
-
-  removeAccount(account: ContactAccount, callback? : (err: Error, succes: ContactAccount[]) => any): Bluebird.Thenable<Contact> {
-    let index: number = this.accounts.indexOf(account);
-    let err: Error = null;
-    if(index === -1) {
-      this.accounts.splice(0, 1, account);
-      this.nicknames.splice(0, 1, account.contactName);
-    } else {
-      err = new Error("This account does not exist for this contact.");
-    }
-    if(callback) {
-      callback(err, this.accounts);
-    }
 	  return Bluebird.resolve(this);
   }
 }
