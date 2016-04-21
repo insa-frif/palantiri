31386bd5a2e3135a6641f8e0b5d59fe1bbdd887c
Sn0wFox <ruben.pericas@laposte.net>
2016-04-21 19:56:55 +0200

    Updated PalantiriDiscussion

--- src/discussion.ts
+++ src/discussion.ts
@@ -1,13 +1,17 @@
 import * as Bluebird from "bluebird";
 
-import {User} from "palantiri-interfaces";
 import {Discussion} from "palantiri-interfaces";
-import {ContactAccount} from "palantiri-interfaces";
-import {GroupAccount} from "palantiri-interfaces";
+import {DiscussionAuthorization} from "palantiri-interfaces";
+import {Contact} from "palantiri-interfaces";
 import {Message} from "palantiri-interfaces";
+import {UserAccount} from "palantiri-interfaces";
 import {utils} from "palantiri-interfaces";
 
-export class OChatDiscussion implements Discussion {
+export class PalantiriDiscussion implements Discussion {
+	protocol: string;
+
+	localDiscussionID: number;
+
   creationDate: Date;
 
   name: string;
@@ -18,11 +22,17 @@ export class OChatDiscussion implements Discussion {
 
   description: string;
 
-  participants: GroupAccount[];
+  participants: Contact[];
 
-  owner: User;
+  owner: UserAccount;
+
+	authorizations: DiscussionAuthorization;
 
   settings: utils.Dictionary<any>;
+
+	isCompatibleWith(protocol: string): boolean {
+		return this.protocol.toLowerCase() === protocol.toLowerCase();
+	}
 
   getMessages(maxMessages: number, afterDate?: Date, filter?: (msg: Message) => boolean): Bluebird<Message[]> {
     // TODO : this depends on how we manage heterogeneous ContactAccount
@@ -36,123 +46,86 @@ export class OChatDiscussion implements Discussion {
 
   sendMessage(msg: Message, callback?: (err: Error, succes: Message) => any): Bluebird.Thenable<Discussion> {
     let err: Error = null;
-    for(let recipient of this.participants) {
-      let gotIt: boolean = false;
-      for(let ownerAccount of this.owner.accounts) {
-        if(ownerAccount.protocol.toLowerCase() === recipient.protocol.toLowerCase()) {
-          let hasAllAccounts: boolean = true;
-          for(let recipAccount of recipient.members) {
-            if(!ownerAccount.hasContactAccount(recipAccount)) {
-              hasAllAccounts = false;
-              break;
-            }
-          }
-          if(hasAllAccounts) {
-            ownerAccount.sendMessageTo(recipient, msg, callback);
-            gotIt = true;
-          }
-        }
-      }
-      if(!err && !gotIt) {
-        err = new Error("At least one recipient could not be served.");
-      }
-    }
+    this.owner.sendMessageTo(this.participants, msg, (error, mess) => {
+	    if(error) {
+		    err = error;
+	    }
+    });
 	  if(callback) {
 		  callback(err, msg);
 	  }
     return Bluebird.resolve(this);
   }
 
-  addParticipants(p: GroupAccount): Bluebird<Discussion> {
-    if(this.participants.indexOf(p) === -1) {
-      let param: string[] = [p.protocol];
-      this.owner.getAccounts(param).then((ownerAccounts) => {
-        let compatibleParticipants: GroupAccount[] = [];
-        for(let participant of this.participants) {
-          if(participant.protocol === p.protocol) {
-            compatibleParticipants.push(participant);
-          }
-        }
-        let gotIt: boolean = false;
-        for(let compatibleParticipant of compatibleParticipants) {
-          for(let ownerAccount of ownerAccounts) {
-            if(ownerAccount.hasContactAccount(compatibleParticipant.members[0])) {
-              // Ok, we have determined which one of the user's accounts
-              // owns the current compatible participant.
-              // Now if it owns the ContactAccounts that we want to add
-              // to this discussion too, we win.
-              if(ownerAccount.hasContactAccount(p.members[0])) {
-                // That's it, we win !
-	              // TODO : well, almost. We need to check if every member is accessible,
-	              //        or it could lead to some problems.
-                ownerAccount.getOrCreateConnection()
-                  .then((co) => {
-                    return co.getConnectedApi();
-                  })
-                  .then((api) => {
-                    api.addMembersToGroupChat(p.members, compatibleParticipant, (err) => {
-                      if(!err) {
-                        compatibleParticipant.addMembers(p.members);
-                      }
-                    });
-                  });
-                gotIt = true;
-                break;
-              }
-            }
-          }
-          if(gotIt) {
-            break;
-          }
-        }
-        // In the case where we still not have been able to add these participants,
-        // there is two solutions :
-        if(!gotIt) {
-          if(compatibleParticipants.length === 0) {
-            // First, we are trying to add accounts using a protocol which is
-            // not in this discussion yet. We just have to add these participants
-            // to this discussion, which will become heterogeneous.
-            this.participants.push(p);
-            this.heterogeneous = true;
-          } else {
-            // Second, we are trying to add accounts from an UserAccount which has
-            // no current contacts in this discussion. We just have to add them.
-            this.participants.push(p);
-          }
-          // TODO : but how the new participants will know that they are in this discussion ?
-          //        For the moment, they won't know until we send a message to them.
-          //        I don't think that it is a real problem.
-          //        If it is, we coud just auto-send a message to them.
-        }
-      });
-    }
+  addParticipants(p: Contact[]): Bluebird<Discussion> {
+	  let that = this;
+	  let error: Error = null;
+	  for(let part of p) {
+		  if(this.participants.indexOf(part) === -1) {
+			  if(this.owner.connection && this.owner.connection.connected) {
+				  this.owner.connection.getConnectedApi()
+					  .then((api) => {
+						  let members: Contact[] = [part];
+						  return api.addMembersToDiscussion(members, that, (err) => {
+							  if(err) {
+								  console.log("Can't add Contact " + part.fullname +":");
+								  console.log(err);
+								  error = err;
+							  }
+						  });
+				    })
+					  .then((disc) => {
+							if(!error) {
+								that.participants.push(part);
+							}
+					  });
+			  }
+		  }
+	  }
     return Bluebird.resolve(this);
   }
 
-  removeParticipants(contactAccount: ContactAccount): Bluebird<Discussion> {
-    // TODO
+  removeParticipants(contact: Contact[]): Bluebird<Discussion> {
+	  let that = this;
+	  let error: Error = null;
+	  for(let part of contact) {
+		  if(this.participants.indexOf(part) === -1) {
+			  if(this.owner.connection && this.owner.connection.connected) {
+				  this.owner.connection.getConnectedApi()
+					  .then((api) => {
+						  let members: Contact[] = [part];
+						  return api.removeMembersFromDiscussion(members, that, (err) => {
+							  if(err) {
+								  console.log("Can't remove Contact " + part.fullname +" from this discussion:");
+								  console.log(err);
+								  error = err;
+							  }
+						  });
+					  })
+					  .then((disc) => {
+						  if(!error) {
+							  that.participants.splice(0, 1, part);
+						  }
+					  });
+			  }
+		  }
+	  }
     return Bluebird.resolve(this);
   }
 
-  getParticipants(): Bluebird<GroupAccount[]> {
+  getParticipants(): Bluebird<Contact[]> {
     return Bluebird.resolve(this.participants);
   }
 
-  onMessage(callback: (msg: Message) => any): Bluebird<Discussion> {
-    // TODO : see troubles in interfaces.ts before
-    return undefined;
-
+  getName(): string {
+    return this.name;
   }
 
-  getName(): Bluebird<string> {
-    return Bluebird.resolve(this.name);
+  getDescription(): string {
+    return this.description;
   }
 
-  getDescription(): Bluebird<string> {
-    return Bluebird.resolve(this.description);
-  }
-
-  getSettings(): Bluebird<utils.Dictionary<any>> {
-    return Bluebird.resolve(this.settings);
+  getSettings(): utils.Dictionary<any> {
+    return this.settings;
   }
 }
